d19d84107a4672af6dd8001b81dd14c29d4bd9e9
YoungKing-Joshua <joshuadeialorse@gmail.com>
2023-02-22 16:47:43 -0800

    Test Project

--- shell.c
+++ shell.c
@@ -1,16 +1,15 @@
 #include "shell.h"
 
-/* global variable for ^C handling */
-unsigned int sig_flag;
-
 /**
  * sig_handler - handles ^C signal interupt
  * @uuv: unused variable (required for signal function prototype)
  *
  * Return: void
  */
+
 static void sig_handler(int uuv)
 {
+	unsigned int sig_flag;
 	(void) uuv;
 	if (sig_flag == 0)
 		_puts("\n$ ");
@@ -28,6 +27,7 @@ static void sig_handler(int uuv)
  */
 int main(int argc __attribute__((unused)), char **argv, char **environment)
 {
+	unsigned int sig_flag;
 	size_t len_buffer = 0;
 	unsigned int is_pipe = 0, i;
 	vars_t vars = {NULL, NULL, NULL, 0, NULL, 0, NULL};
